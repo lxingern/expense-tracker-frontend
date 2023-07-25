72e97e14b70797824ed11424e1bf505ac466dd05
Xing Ern <lxingern@gmail.com>
2023-07-25 16:20:54 +0800

    display loading status when sign in or register form is submitting

--- src/components/Auth/RegisterForm.jsx
+++ src/components/Auth/RegisterForm.jsx
@@ -1,7 +1,11 @@
-import { Form, useActionData } from "react-router-dom"
+import { Form, useActionData, useNavigation } from "react-router-dom"
+import Spinner from "react-bootstrap/Spinner";
 
 const RegisterForm = () => {
   const data = useActionData()
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === "submitting"
 
   return (
     <Form method="post">
@@ -16,7 +20,14 @@ const RegisterForm = () => {
       <label htmlFor="password" className="form-label">Password</label>
       <input name="password" id="password" type="password" className="form-control mb-3"></input>
 
-      <button className="btn btn-primary">Register</button>
+      <button className="btn btn-primary" disabled={isSubmitting}>
+        { isSubmitting ? "Registering... " : "Register" } 
+        { isSubmitting && (
+          <Spinner animation="border" role="status" size="sm">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        )}
+      </button>
     </Form>
   )
 }
--- src/components/Auth/SignInForm.jsx
+++ src/components/Auth/SignInForm.jsx
@@ -1,7 +1,11 @@
-import { Form, useActionData } from "react-router-dom"
+import { Form, useActionData, useNavigation } from "react-router-dom"
+import Spinner from "react-bootstrap/Spinner";
 
 const SignInForm = () => {
   const data = useActionData()
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === "submitting"
 
   return (
     <Form method="post">
@@ -13,7 +17,14 @@ const SignInForm = () => {
       <label htmlFor="password" className="form-label">Password</label>
       <input name="password" id="password" type="password" className="form-control mb-3"></input>
 
-      <button className="btn btn-primary">Sign in</button>
+      <button className="btn btn-primary" disabled={isSubmitting}>
+        { isSubmitting ? "Signing in... " : "Sign in" } 
+        { isSubmitting && (
+          <Spinner animation="border" role="status" size="sm">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        )}
+      </button>
     </Form>
   )
 }
